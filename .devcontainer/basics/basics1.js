a7179daf24b970a8e8ad979fd95ce531e2d316f1
NitinYadav-student <nitinyadav273151@gmail.com>
2025-06-18 04:48:17 +0000

    container config done

--- .devcontainer/basics/basics1.js
+++ .devcontainer/basics/basics1.js
@@ -3,5 +3,5 @@ let name = "nitin";
 let age = 21;
 let standard = "BCA";
 console.log(name);
-console.log([name , age , standard]);
+console.table([name , age , standard]);
 console.log("NITIN YADAV");
